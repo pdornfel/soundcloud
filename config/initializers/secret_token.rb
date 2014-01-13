9b9a04bb7fa80305ec96928650a372c6bdf9f9f6
Paul Dornfeld <pdornfel@gmail.com>
2014-01-13 17:09:32 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soundcloud::Application.config.secret_key_base = '00124c3482e33211eefe872c6c028ddcd4cf54105512fa394267579dc94098aac71b5024d886fc07e38f1aa322977dbfd59c73aa5a9345ad6e38b0943ead99f4'
